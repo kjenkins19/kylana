4ba6a301c0517eb6823fe3c4a03b16bbc463f5b3
kylejenkins <kylej19@gmail.com>
2019-06-17 21:39:16 -0400

    Defining Styles

--- src/components/layout.js
+++ src/components/layout.js
@@ -7,12 +7,12 @@
 
 import React from "react"
 import PropTypes from "prop-types"
-import { StaticQuery, graphql } from "gatsby"
+import {StaticQuery, graphql} from "gatsby"
 
 import Header from "./header"
 import "./layout.css"
 
-const Layout = ({ children }) => (
+const Layout = ({children}) => (
   <StaticQuery
     query={graphql`
       query SiteTitleQuery {
@@ -25,7 +25,7 @@ const Layout = ({ children }) => (
     `}
     render={data => (
       <>
-        <Header siteTitle={data.site.siteMetadata.title} />
+        <Header siteTitle={data.site.siteMetadata.title}/>
         <div
           style={{
             margin: `0 auto`,
@@ -44,10 +44,10 @@ const Layout = ({ children }) => (
       </>
     )}
   />
-)
+);
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
-}
+};
 
 export default Layout
---
+++ src/components/styles.js
@@ -0,0 +1,9 @@
+const styles = {
+  primary0: "#1097FF",
+  primary1: "#A9DAFF",
+  primary2: "#79C5FF",
+  primary3: "#004F8B",
+  primary4: "#003D6B"
+};
+
+export {styles};
